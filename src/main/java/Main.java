633af8b497783a643d8d1638fde4886f5852b624
luca <luca1230@gmail.com>
2020-11-30 18:09:09 -0800

    initial commit, a bit late and added a lot of work, mostly stuff found on the lwjgl website

---
+++ src/main/java/Main.java
@@ -0,0 +1,88 @@
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class Main implements Runnable {
+
+    private final static int width = 1280;
+    private final static int height = 720;
+
+    private Thread thread;
+    private boolean running;
+    private long window;
+
+    public void start() {
+        running = true;
+        thread = new Thread(this, "Game");
+        thread.start();
+    }
+
+    public void run() {
+        init();
+        while (running) {
+            update();
+            render();
+
+            if (glfwWindowShouldClose(window)) {
+                running = false;
+            }
+        }
+    }
+
+    private void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        if (window == NULL) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+        glfwShowWindow(window);
+    }
+
+    private void update() {
+        glfwPollEvents();
+    }
+
+    private void render() {
+        glfwSwapBuffers(window);
+    }
+
+    public static void main(String[] args) {
+        new Main().start();
+    }
+}
